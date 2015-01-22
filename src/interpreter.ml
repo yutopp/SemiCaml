06af57617e3f986c6a63ac58e0468fe9abd4e482
iyahoo <s1200191@gmail.com>
2015-01-22 09:55:09 +0900

    fix test form

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,13 +1,13 @@
 open Ast
-
+open Analyzer
 (* todo *)
-       
+
 type value =
   | IntVal of int
-  | FloatVal of float                  
+  | FloatVal of float
   | BoolVal of bool
 
-let printer values = match values with
+let val_to_str values = match values with
   | IntVal n -> Printf.sprintf "%d\n" n
   | FloatVal n -> Printf.sprintf "%f\n" n
   | BoolVal b -> Printf.sprintf "%b\n" b
@@ -21,23 +21,23 @@ type constant_folder = {
 let emptyenv () = Hashtbl.create 10
 
 let env_ext env x v = Hashtbl.add env x v; env
-                                  
-let lookup x env = Hashtbl.find env x                                
-                         
-let rec eval input env =
-  let intop f e1 e2 = match (eval e1 env, eval e2 env) with
+
+let lookup x env = Hashtbl.find env x
+
+let rec eval' input env =
+  let intop f e1 e2 = match (eval' e1 env, eval' e2 env) with
     | (IntVal n1, IntVal n2) -> IntVal (f n1 n2)
     | _ -> failwith "Integer value expected"
   in
-  let floatop f e1 e2 = match (eval e1 env, eval e2 env) with
+  let floatop f e1 e2 = match (eval' e1 env, eval' e2 env) with
     | (FloatVal n1, FloatVal n2) -> FloatVal (f n1 n2)
     | _ -> failwith "float value expected"
   in
-  let boolop f e1 e2 = match (eval e1 env, eval e2 env) with
+  let boolop f e1 e2 = match (eval' e1 env, eval' e2 env) with
     | (BoolVal b1, BoolVal b2) -> BoolVal (f b1 b2)
     | _ -> failwith "bool value expected"
   in
-  let compop f e1 e2 = match (eval e1 env, eval e2 env) with
+  let compop f e1 e2 = match (eval' e1 env, eval' e2 env) with
     | (IntVal n1, IntVal n2) -> BoolVal (f.int n1 n2)
     | (FloatVal n1, FloatVal n2) -> BoolVal (f.float n1 n2)
     | (BoolVal b1, BoolVal b2) -> BoolVal (f.bool b1 b2)
@@ -54,122 +54,39 @@ let rec eval input env =
   | FloatLiteral n -> FloatVal n
   | BoolLiteral b -> BoolVal b
   | Id id -> lookup id env
-  | VerDecl (id,e1,Some e2) -> eval e2 (env_ext env id (eval e1 env))
-  | VerDecl (id,e1,None) -> lookup id (env_ext env id (eval e1 env))
-  (* | FuncDecl (id,args,e1,Some e2) = eval e2 (env_ext env id (eval e1 env)) *)
+  | VerDecl (id,e1,Some e2) -> eval' e2 (env_ext env id (eval' e1 env))
+  | VerDecl (id,e1,None) -> lookup id (env_ext env id (eval' e1 env))
+  (* | FuncDecl (id,args,e1,Some e2) = eval' e2 (env_ext env id (eval' e1 env)) *)
   (* | ArrayNew (str,e1) ->  *)
-                                   
+
   | AddIntExpr (e1,e2) -> intop ( + ) e1 e2
   | SubIntExpr (e1,e2) -> intop ( - ) e1 e2
   | MulIntExpr (e1,e2) -> intop ( * ) e1 e2
-  | DivIntExpr (e1,e2) when (eval e2 env = IntVal 0) -> failwith "0 Division"
+  | DivIntExpr (e1,e2) when (eval' e2 env = IntVal 0) -> failwith "0 Division"
   | DivIntExpr (e1,e2) -> intop ( / ) e1 e2
   | AddFloatExpr (e1,e2) -> floatop ( +. ) e1 e2
   | SubFloatExpr (e1,e2) -> floatop ( -. ) e1 e2
   | MulFloatExpr (e1,e2) -> floatop ( *. ) e1 e2
-  | DivFloatExpr (e1,e2) when (eval e2 env = FloatVal 0.0) -> failwith "0 Division"
+  | DivFloatExpr (e1,e2) when (eval' e2 env = FloatVal 0.0) -> failwith "0 Division"
   | DivFloatExpr (e1,e2) -> floatop ( /. ) e1 e2
-                                    
+
   | LogicOrExpr (e1,e2) -> boolop ( || ) e1 e2
-  | LogicAndExpr (e1,e2) -> boolop ( && ) e1 e2                                    
+  | LogicAndExpr (e1,e2) -> boolop ( && ) e1 e2
   | EqualExpr (e1,e2) -> compop ( eq ) e1 e2
   | NotEqualExpr (e1,e2) -> compop ( neq ) e1 e2
   | LessExpr (e1,e2) -> compop ( lt ) e1 e2
   | LessEqualExpr (e1,e2) -> compop ( lte ) e1 e2
   | GreaterExpr (e1,e2) -> compop ( gt ) e1 e2
   | GreaterEqualExpr (e1,e2) -> compop ( gte ) e1 e2
-                                       
+
   | CondExpr (e1,e2,e3) ->
      begin
-       match (eval e1 env) with
-       | BoolVal true -> eval e2 env
-       | BoolVal false -> eval e3 env
+       match (eval' e1 env) with
+       | BoolVal true -> eval' e2 env
+       | BoolVal false -> eval' e3 env
        | _ -> failwith "first exp bool value expected"
      end
-  | _ -> failwith "unknown exp"                  
-              
-let rec interpreter input =
-  eval input (emptyenv ())          
-          
+  | _ -> failwith "unknown exp"
 
-let _ = interpreter (VerDecl ("x", IntLiteral 3,
-                              (Some (VerDecl ("y", IntLiteral 5,
-                                              (Some (VerDecl ("x", IntLiteral 8,
-                                                              (Some (AddIntExpr (Id "x", Id "y")))))))))))
-
-
-          
-let _ = eval (AddIntExpr (IntLiteral 3, IntLiteral 2)) (emptyenv ())                            = IntVal 5                
-let _ = eval (AddIntExpr (AddIntExpr (IntLiteral 3, IntLiteral 3), IntLiteral 2)) (emptyenv ()) = IntVal 8                
-let _ = eval (SubIntExpr (IntLiteral 3, IntLiteral 2)) (emptyenv ())                            = IntVal 1                
-let _ = eval (MulIntExpr (IntLiteral 3, IntLiteral 2)) (emptyenv ())                            = IntVal 6                
-let _ = eval (SubIntExpr (MulIntExpr (IntLiteral 3, IntLiteral 2), IntLiteral 2)) (emptyenv ()) = IntVal 4                
-let _ = eval (DivIntExpr (IntLiteral 10, IntLiteral 2)) (emptyenv ())                           = IntVal 5                
-(* let _ = eval (DivIntExpr (IntLiteral 10, IntLiteral 0)) (emptyenv ()) (\* Exception "0 Division" *\) *)
-                                                                                                  
-let _ = eval (AddFloatExpr (FloatLiteral 3.0, FloatLiteral 2.0)) (emptyenv ())                  = FloatVal 5.             
-let _ = eval (AddFloatExpr (AddFloatExpr(FloatLiteral 3., FloatLiteral 3.),                       
-                            FloatLiteral 2.)) (emptyenv ())                                     = FloatVal 8.             
-let _ = eval (SubFloatExpr (FloatLiteral 3., FloatLiteral 2.)) (emptyenv ())                    = FloatVal 1.             
-let _ = eval (MulFloatExpr (FloatLiteral 3., FloatLiteral 2.)) (emptyenv ())                    = FloatVal 6.             
-let _ = eval (DivFloatExpr (FloatLiteral 10., FloatLiteral 2.)) (emptyenv ())                   = FloatVal 5.             
-(* let _ = eval (DivFloatExpr (FloatLiteral 10., FloatLiteral 0.)) (emptyenv ()) (\* Exception "0 Division" *\) *)
-                                                                                                  
-let _ = eval (EqualExpr (IntLiteral 3, IntLiteral 3)) (emptyenv ())                             = BoolVal true            
-let _ = eval (EqualExpr (IntLiteral 3, IntLiteral 2)) (emptyenv ())                             = BoolVal false           
-let _ = eval (EqualExpr (FloatLiteral 3., FloatLiteral 3.)) (emptyenv ())                       = BoolVal true            
-let _ = eval (EqualExpr (FloatLiteral 3., FloatLiteral 2.)) (emptyenv ())                       = BoolVal false           
-let _ = eval (EqualExpr (BoolLiteral true, BoolLiteral false)) (emptyenv ())                    = BoolVal false           
-let _ = eval (EqualExpr (BoolLiteral true, BoolLiteral true)) (emptyenv ())                     = BoolVal true             
-(* let _ = eval (EqualExpr (IntLiteral 3, BoolLiteral true)) (emptyenv ())  (\* Exceptino Integer, Float or Bool values expected *\) *)
-
-let _ = eval (NotEqualExpr (IntLiteral 3, IntLiteral 3)) (emptyenv ())                          = BoolVal false 
-let _ = eval (NotEqualExpr (IntLiteral 3, IntLiteral 2)) (emptyenv ())                          = BoolVal true 
-let _ = eval (NotEqualExpr (FloatLiteral 3., FloatLiteral 3.)) (emptyenv ())                    = BoolVal false
-let _ = eval (NotEqualExpr (FloatLiteral 3., FloatLiteral 2.)) (emptyenv ())                    = BoolVal true 
-let _ = eval (NotEqualExpr (BoolLiteral true, BoolLiteral false)) (emptyenv ())                 = BoolVal true 
-let _ = eval (NotEqualExpr (BoolLiteral true, BoolLiteral true)) (emptyenv ())                  = BoolVal false
-
-let _ = eval (LessExpr (IntLiteral 3, IntLiteral 5)) (emptyenv ())                              = BoolVal true 
-let _ = eval (LessExpr (IntLiteral 5, IntLiteral 3)) (emptyenv ())                              = BoolVal false
-let _ = eval (LessExpr (BoolLiteral false, BoolLiteral true)) (emptyenv ())                     = BoolVal true
-let _ = eval (LessEqualExpr (IntLiteral 3, IntLiteral 3)) (emptyenv ())                         = BoolVal true 
-let _ = eval (LessEqualExpr (IntLiteral 3, IntLiteral 5)) (emptyenv ())                         = BoolVal true 
-let _ = eval (LessEqualExpr (IntLiteral 5, IntLiteral 3)) (emptyenv ())                         = BoolVal false
-let _ = eval (GreaterExpr (IntLiteral 5, IntLiteral 3)) (emptyenv ())                           = BoolVal true 
-let _ = eval (GreaterExpr (IntLiteral 3, IntLiteral 5)) (emptyenv ())                           = BoolVal false
-let _ = eval (GreaterExpr (BoolLiteral true, BoolLiteral false)) (emptyenv ())                  = BoolVal true
-let _ = eval (GreaterEqualExpr (IntLiteral 3, IntLiteral 3)) (emptyenv ())                      = BoolVal true 
-let _ = eval (GreaterEqualExpr (IntLiteral 5, IntLiteral 3)) (emptyenv ())                      = BoolVal true 
-let _ = eval (GreaterEqualExpr (IntLiteral 3, IntLiteral 5)) (emptyenv ())                      = BoolVal false 
-                                                                  
-let _ = eval (LogicOrExpr (BoolLiteral true, BoolLiteral true)) (emptyenv ())                   = BoolVal true  
-let _ = eval (LogicOrExpr (BoolLiteral false, BoolLiteral true)) (emptyenv ())                  = BoolVal true 
-let _ = eval (LogicAndExpr (BoolLiteral true, BoolLiteral true)) (emptyenv ())                  = BoolVal true 
-let _ = eval (LogicAndExpr (BoolLiteral false, BoolLiteral true)) (emptyenv ())                 = BoolVal false
-
-let _ = eval (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 3),
-                        (MulIntExpr (IntLiteral 2, IntLiteral 3)),
-                        (DivIntExpr (IntLiteral 10, IntLiteral 2)))) (emptyenv ())              = IntVal 6
-                                                                                             
-let _ = eval (CondExpr (GreaterExpr (IntLiteral 3, IntLiteral 3),                            
-                        (MulIntExpr (IntLiteral 2, IntLiteral 3)),                           
-                        (DivIntExpr (IntLiteral 10, IntLiteral 2)))) (emptyenv ())              = IntVal 5
-
-let _ = eval (AddIntExpr (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 3),
-                                    (MulIntExpr (IntLiteral 2, IntLiteral 2)),
-                                    (DivIntExpr (IntLiteral 10, IntLiteral 2))),
-                          IntLiteral 3)) (emptyenv ())                
-
-let _ = eval (AddIntExpr (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 2),
-                                    (MulIntExpr (IntLiteral 2, IntLiteral 2)),
-                                    (DivIntExpr (IntLiteral 10, IntLiteral 2))),
-                          IntLiteral 3)) (emptyenv ())
-             
-(* let _ = Analyzer.analyze (Program [FuncDecl ("func", *)
-(*                                     ["a";"b";], *)
-(*                                     AddIntExpr(Id "a", *)
-(*                                                Id "b"), *)
-(*                                     Some (FuncCall ("func", *)
-(*                                                     [IntLiteral 3;IntLiteral 5])));]);; *)
-  
+let rec eval input =
+  eval' input (emptyenv ())
--- test/tester.ml
+++ test/tester.ml
@@ -1,54 +1,147 @@
 open OUnit2
 open Ast
 open Token
-(* todo *)
-(* lexer, perser, <- string *)
-(* code gen <- *)
-(* analyze' <- ast *)
-
-let test_inter1 test_ctxt =
-  assert_equal ~msg:"Interpreter 1"
-               ~printer:Interpreter.printer
-               (Interpreter.IntVal 3)
-               (Interpreter.interpreter
-                  (VerDecl ("x", IntLiteral 3, None)))
-               
-let test_inter2 test_ctxt =
-  assert_equal ~msg:"Interpreter 2"
-               ~printer:Interpreter.printer
-               (Interpreter.IntVal 6)
-               (Interpreter.interpreter
-                  (VerDecl ("x", IntLiteral 3, (Some (AddIntExpr(Id "x", IntLiteral 3))))))
-               
-let test_inter3 test_ctxt =
-  assert_equal ~msg:"Interpreter 3"
-               ~printer:Interpreter.printer
-               (Interpreter.IntVal 13)
-               (Interpreter.interpreter
-                  (VerDecl
-                     ("x", IntLiteral 3,
-                      (Some (VerDecl
-                               ("y", IntLiteral 5,
-                                (Some (VerDecl
-                                         ("x", IntLiteral 8,
-                                          (Some (AddIntExpr (Id "x", Id "y"))))))))))))
-               
-let test_lex1 test_ctxt =
-  assert_equal ~msg:"Lexer 1"
-               [IntLiteral 1; Op AddInt; IntLiteral 1]
-               (Lexer.lex (Lexer.char_list_of_string "1 + 1"))
-               
-let test_lex2 test_ctxt =
-  assert_equal ~msg:"Lexer 2"
-               [Keyword Let; Identifier "x"; Op Assign; IntLiteral 3]
-               (Lexer.lex (Lexer.char_list_of_string "let x = 3"))
+open Interpreter
 
 let suite =
-  "suite" >::: ["eval test" >::: ["interpreter test 1" >:: test_inter1;
-                                  "interpreter test 2" >:: test_inter2;
-                                  "interpreter test 3" >:: test_inter3;];
-                "lexer test" >::: ["lexer test 1" >:: test_lex1;
-                                   "lexer test 2" >:: test_lex2];]
-                 
-let run_test = run_test_tt_main suite
+  "suite" >::: ["eval test" >::: (List.map
+                                    (fun (title,res,arg) ->
+                                     title >::
+                                       (fun test_ctxt ->
+                                        assert_equal ~printer:Interpreter.val_to_str
+                                          res (Interpreter.eval arg)))
 
+                                    ["Interpreter 1", IntVal 3,
+                                     (VerDecl ("x", IntLiteral 3, None));
+                                     "Interpreter 2", IntVal 6,
+                                     (VerDecl ("x", IntLiteral 3, (Some (AddIntExpr(Id "x", IntLiteral 3)))));
+                                     "Interpreter 3", IntVal 13,
+                                     (VerDecl
+                                        ("x", IntLiteral 3,
+                                         (Some (VerDecl
+                                                  ("y", IntLiteral 5,
+                                                   (Some (VerDecl
+                                                            ("x", IntLiteral 8,
+                                                             (Some (AddIntExpr (Id "x", Id "y")))))))))));
+                                     "Interpreter 4", IntVal 5,
+                                     (AddIntExpr (IntLiteral 3, IntLiteral 2));
+                                     "Interpreter 5", IntVal 8,
+                                     (AddIntExpr (AddIntExpr (IntLiteral 3, IntLiteral 3), IntLiteral 2));
+                                     "Interpreter 6", IntVal 1,
+                                     (SubIntExpr (IntLiteral 3, IntLiteral 2));
+                                     "Interpreter 7", IntVal 6,
+                                     (MulIntExpr (IntLiteral 3, IntLiteral 2));
+                                     "Interpreter 8", IntVal 4,
+                                     (SubIntExpr (MulIntExpr (IntLiteral 3, IntLiteral 2), IntLiteral 2));
+                                     "Interpreter 9", IntVal 5,
+                                     (DivIntExpr (IntLiteral 10, IntLiteral 2));
+                                     "Interpreter 10", FloatVal 5.,
+                                     (AddFloatExpr (FloatLiteral 3.0, FloatLiteral 2.0));
+                                     "Interpreter 11", FloatVal 8.,
+                                     (AddFloatExpr (AddFloatExpr(FloatLiteral 3., FloatLiteral 3.),
+                                                    FloatLiteral 2.));
+                                     "Interpreter 12", FloatVal 1.,
+                                     (SubFloatExpr (FloatLiteral 3., FloatLiteral 2.));
+                                     "Interpreter 13", FloatVal 6.,
+                                     (MulFloatExpr (FloatLiteral 3., FloatLiteral 2.));
+                                     "Interpreter 14", FloatVal 5.,
+                                     (DivFloatExpr (FloatLiteral 10., FloatLiteral 2.));
+                                     "Interpreter 15", BoolVal true,
+                                     (EqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 16", BoolVal false,
+                                     (EqualExpr (IntLiteral 3, IntLiteral 2));
+                                     "Interpreter 17", BoolVal true,
+                                     (EqualExpr (FloatLiteral 3., FloatLiteral 3.));
+                                     "Interpreter 18", BoolVal false,
+                                     (EqualExpr (FloatLiteral 3., FloatLiteral 2.));
+                                     "Interpreter 19", BoolVal false,
+                                     (EqualExpr (BoolLiteral true, BoolLiteral false));
+                                     "Interpreter 20", BoolVal true,
+                                     (EqualExpr (BoolLiteral true, BoolLiteral true));
+                                     "Interpreter 21", BoolVal false,
+                                     (NotEqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 22", BoolVal false,
+                                     (NotEqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 23", BoolVal true,
+                                     (NotEqualExpr (IntLiteral 3, IntLiteral 2));
+                                     "Interpreter 24", BoolVal false,
+                                     (NotEqualExpr (FloatLiteral 3., FloatLiteral 3.));
+                                     "Interpreter 25", BoolVal true,
+                                     (NotEqualExpr (FloatLiteral 3., FloatLiteral 2.));
+                                     "Interpreter 26", BoolVal true,
+                                     (NotEqualExpr (BoolLiteral true, BoolLiteral false));
+                                     "Interpreter 26", BoolVal false,
+                                     (NotEqualExpr (BoolLiteral true, BoolLiteral true));
+                                     "Interpreter 27", BoolVal true ,
+                                     (LessExpr (IntLiteral 3, IntLiteral 5));
+                                     "Interpreter 28", BoolVal false,
+                                     (LessExpr (IntLiteral 5, IntLiteral 3));
+                                     "Interpreter 29", BoolVal true,
+                                     (LessExpr (BoolLiteral false, BoolLiteral true));
+                                     "Interpreter 30", BoolVal true,
+                                     (LessEqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 31", BoolVal true,
+                                     (LessEqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 32", BoolVal true,
+                                     (LessEqualExpr (IntLiteral 3, IntLiteral 5));
+                                     "Interpreter 33", BoolVal false,
+                                     (LessEqualExpr (IntLiteral 5, IntLiteral 3));
+                                     "Interpreter 34", BoolVal true,
+                                     (GreaterExpr (IntLiteral 5, IntLiteral 3));
+                                     "Interpreter 35", BoolVal false,
+                                     (GreaterExpr (IntLiteral 3, IntLiteral 5));
+                                     "Interpreter 36", BoolVal true,
+                                     (GreaterExpr (BoolLiteral true, BoolLiteral false));
+                                     "Interpreter 37", BoolVal true,
+                                     (GreaterEqualExpr (IntLiteral 3, IntLiteral 3));
+                                     "Interpreter 38", BoolVal true,
+                                     (GreaterEqualExpr (IntLiteral 5, IntLiteral 3));
+                                     "Interpreter 39", BoolVal true,
+                                     (GreaterEqualExpr (IntLiteral 5, IntLiteral 3));
+                                     "Interpreter 40", BoolVal false,
+                                     (GreaterEqualExpr (IntLiteral 3, IntLiteral 5));
+                                     "Interpreter 41", BoolVal true,
+                                     (LogicOrExpr (BoolLiteral true, BoolLiteral true));
+                                     "Interpreter 42", BoolVal true,
+                                     (LogicOrExpr (BoolLiteral false, BoolLiteral true));
+                                     "Interpreter 43", BoolVal true,
+                                     (LogicAndExpr (BoolLiteral true, BoolLiteral true));
+                                     "Interpreter 44", BoolVal false,
+                                     (LogicAndExpr (BoolLiteral false, BoolLiteral true));
+                                     "Interpreter 45", IntVal 6,
+                                     (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 3),
+                                                (MulIntExpr (IntLiteral 2, IntLiteral 3)),
+                                                (DivIntExpr (IntLiteral 10, IntLiteral 2))));
+                                     "Interpreter 46", IntVal 5,
+                                     (CondExpr (GreaterExpr (IntLiteral 3, IntLiteral 3),
+                                                (MulIntExpr (IntLiteral 2, IntLiteral 3)),
+                                                (DivIntExpr (IntLiteral 10, IntLiteral 2))));
+                                     "Interpreter 46", IntVal 7,
+                                     (AddIntExpr (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 3),
+                                                            (MulIntExpr (IntLiteral 2, IntLiteral 2)),
+                                                            (DivIntExpr (IntLiteral 10, IntLiteral 2))),
+                                                  IntLiteral 3));
+                                     "Interpreter 47", IntVal 8,
+                                     (AddIntExpr (CondExpr (EqualExpr (IntLiteral 3, IntLiteral 2),
+                                                            (MulIntExpr (IntLiteral 2, IntLiteral 2)),
+                                                            (DivIntExpr (IntLiteral 10, IntLiteral 2))),
+                                                  IntLiteral 3));
+                                    ]);
+                "lexer test" >::: (List.map
+                                     (fun (title,res,arg) ->
+                                      title >::
+                                        (fun test_ctxt ->
+                                         assert_equal
+                                           res
+                                           (Lexer.lex (Lexer.char_list_of_string arg))))
+                                     ["Lexer 1", [IntLiteral 1; Op AddInt; IntLiteral 1],
+                                      "1 + 1";
+                                      "Lexer 2", [Keyword Let; Identifier "x"; Op Assign; IntLiteral 3],
+                                      "let x = 3";
+                                     ]);
+                (* "perser test"; *)
+                (* "analyze test"; *)
+                (* "codegen test"; *)
+               ]
+
+let run_test = run_test_tt_main suite
