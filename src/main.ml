6f8804a55da2d95a3c049dca4c1f9f771e4f1b5a
youxkei <youxkei@gmail.com>
2015-01-26 01:15:13 +0900

    load test.seml

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,16 @@
+let rec char_list_of_in_channel in_channel = 
+  try
+    let read_char = input_char in_channel in read_char :: char_list_of_in_channel in_channel
+  with End_of_file -> []
 
-(*
 let () =
   Printf.printf "mean mean\n";
-  let ast = Parser.parse "test.seml" in
+  let in_channel = open_in "test.seml" in
+  let chars = char_list_of_in_channel in_channel in
+  let tokens = Lexer.lex chars in
+  let ast = Parser.parse tokens in
   Parser.dump ast;
   let attr_ast = Analyzer.analyze ast in
   flush stdout;
   let llvm_module = Codegen.compile attr_ast in
   Codegen.create_executable llvm_module
-  *)
