515b92cec366dd774df1ba1f55e57d45f44f642e
iyahoo <s1200191@gmail.com>
2015-01-27 00:45:46 +0900

    Fix about argument in main

--- src/main.ml
+++ src/main.ml
@@ -14,10 +14,9 @@ let repl () =
   repl' ()                        
 
 let () =
-  let arg = Sys.argv.(1) in
-  match arg with
-  | "--repl" -> repl ()
-  | _ ->
+  let argv_len = Array.length Sys.argv in
+  match argv_len with
+  | 1 -> 
      Printf.printf "mean mean\n";
      let in_channel = open_in "test.seml" in
      let chars = char_list_of_in_channel in_channel in
@@ -28,3 +27,12 @@ let () =
      flush stdout;
      let llvm_module = Codegen.compile attr_ast in
      Codegen.create_executable llvm_module
+  | 2 -> 
+     let arg = Sys.argv.(1) in
+     begin
+       match arg with
+       | "--repl" -> repl ()
+       | _ -> failwith "invalid argument"
+     end
+  | _ -> failwith "invalid argument"
+
