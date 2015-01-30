ecbf0300eb338230fefba25fc59c104107cd4a8f
youxkei <youxkei@gmail.com>
2015-01-31 00:14:03 +0900

    Use Ast.dump instead of Parser.dump

--- src/main.ml
+++ src/main.ml
@@ -28,7 +28,7 @@ let () =
      let chars = char_list_of_in_channel in_channel in
      let tokens = Lexer.lex chars in
      let ast = Parser.parse tokens in
-     Parser.dump ast;
+     Ast.dump ast;
      let attr_ast = Analyzer.analyze ast in
      flush stdout;
      let llvm_module = Codegen.compile attr_ast in
